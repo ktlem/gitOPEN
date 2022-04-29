9e57a3d2e788bf4bd2aec16f3a00c5e5a15f0c7b
Kristian Lempinen <kristian.lempinen@tuni.fi>
2022-04-29 17:20:34 +0300

    Hello from feature

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
 print("Hello World!")
-=======
-print("Hello world!")
-<<<<<<< HEAD
->>>>>>> inter/master
-=======
-<<<<<<< HEAD
->>>>>>> course/master
-=======
 print("Hello from feature")
->>>>>>> feature/create-awesome
->>>>>>> 70e995bdedea8296140bdbfab87e3351da924836
+
