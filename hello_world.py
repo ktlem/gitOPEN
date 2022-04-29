b1b65f2bf1b71a841adbdccfe5aabace0edd813d
Kristian Lempinen <kristian.lempinen@tuni.fi>
2022-04-29 17:37:33 +0300

    Fix merge conflicts

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,4 @@
 print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("Hello once again")
-=======
 print("Hello darkness my old friend")
->>>>>>> conflict/master
-=======
 print("I've come to talk with you again")
->>>>>>> conflict/feature/modify-print
